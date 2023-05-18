0d755bfc93c408e3fb5ac10656a70ddbaef740b0
Davi Costa <davifncosta@gmail.com>
2023-05-18 12:10:27 -0300

    segundo exercicio do slide concluido

--- funcoes/atividades_slides/atividade2/solucao1/src/App.java
+++ funcoes/atividades_slides/atividade2/solucao1/src/App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        obrigatoriedade(72);
+    }
+    
+    public static void obrigatoriedade (int idade){
+        if (idade >= 0 && idade <= 15){
+            System.out.println("Sem permissão para votar");
+        } else if ((idade >= 16 && idade <= 17) || idade > 65){
+            System.out.println("Voto facultativo");
+        } else if (idade >= 18 && idade <= 65) {
+            System.out.println("Voto obrigatório");
+        } else {
+            System.out.println("Idade não existente!");
+        }
     }
 }
