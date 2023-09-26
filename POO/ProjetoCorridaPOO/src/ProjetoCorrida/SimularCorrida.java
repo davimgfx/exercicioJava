9d625ab7e697d8f144a1ddc165554d0bdc4e51f3
Davi Costa <davifncosta@gmail.com>
2023-09-26 19:57:55 -0300

    add projeto corrida poo

---
+++ POO/ProjetoCorridaPOO/src/ProjetoCorrida/SimularCorrida.java
@@ -0,0 +1,70 @@
+package ProjetoCorrida;
+
+public class SimularCorrida {
+    public static void main(String[] args) {
+        Veiculo carro1 = new Veiculo("Carro 1");
+        Veiculo carro2 = new Veiculo("Carro 2");
+        int distanciaMaxima = 50;
+        
+        System.out.println("INICIO DA CORRIDA!");
+        System.out.println("--------------------------------------------------");
+        System.out.println("#");
+        System.out.println("#");
+        System.out.println("--------------------------------------------------");
+        
+        while (carro1.getDistancia() < distanciaMaxima && carro2.getDistancia() < distanciaMaxima) {
+            carro1.mover();
+            carro2.mover();
+            
+            imprimirCorrida(carro1, carro2);
+        }
+        resultadoCorrida(carro1, carro2);
+    }
+    
+    
+    public static void imprimirCorrida(Veiculo carro1, Veiculo carro2) {
+        int distanciaCarro1 = carro1.getDistancia();
+        int distanciaCarro2 = carro2.getDistancia();
+        String representacaoCarro1 = "";
+        String representacaoCarro2 = "";
+        
+        System.out.println("--------------------------------------------------");
+        
+        for (int i = 0; i < distanciaCarro1; i++) {
+            representacaoCarro1 += " ";
+        }
+        
+        representacaoCarro1 += "#";
+         
+        for (int i = 0; i < distanciaCarro2; i++) {
+            representacaoCarro2 += " ";
+        }
+        
+       
+        representacaoCarro2 += "#";
+        
+        System.out.println(representacaoCarro1);
+        System.out.println(representacaoCarro2);
+        
+        System.out.println("--------------------------------------------------");
+        
+        System.out.print(carro1.getNome()+" andou : " + distanciaCarro1);
+        System.out.print(" | ");
+        System.out.println(carro2.getNome()+" andou : " + distanciaCarro2);
+    }
+    
+    public static void resultadoCorrida(Veiculo carro1, Veiculo carro2){
+		int distanciaCarro1 = carro1.getDistancia();
+        int distanciaCarro2 = carro2.getDistancia();
+        
+        if(distanciaCarro1 > distanciaCarro2) {
+        	System.out.println(carro1.getNome()+" venceu a corrida!");
+        } else if ( distanciaCarro2 > distanciaCarro1) {
+        	System.out.println(carro2.getNome()+" venceu a corrida!");
+        } else {
+        	System.out.println("EMPATE!!!");
+        }
+	}
+}
+
+	
