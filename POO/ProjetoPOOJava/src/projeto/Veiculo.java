fba5a8f9cb2875df2d7cd747515d574cdd1986f9
Davi Fernandes <davifncosta@gmail.com>
2023-10-09 20:11:13 -0300

    feat: add the last version in the exercise poo

---
+++ POO/ProjetoPOOJava/src/projeto/Veiculo.java
@@ -0,0 +1,29 @@
+package projeto;
+
+import java.util.Random;
+
+class Veiculo {
+    private int velocidade;
+    private int distancia;
+    private String nome;
+
+    public Veiculo(String nome) {
+        this.nome = nome;
+        this.velocidade = 0;
+        this.distancia = 0;
+    }
+
+    public void mover() {
+        Random random = new Random();
+        this.velocidade = random.nextInt(3) + 1; 
+        this.distancia += this.velocidade;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
